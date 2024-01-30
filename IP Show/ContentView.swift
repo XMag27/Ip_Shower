90a57782309f592f1215aa517ba4945b34cf154f
XMag27 <xmagalla@espol.edu.ec>
2024-01-29 21:31:26 -0500

    Initial Commit

---
+++ IP Show/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IP Show
+//
+//  Created by Xavier Magallanes on 29/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IP Show/IP_ShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  IP_ShowApp.swift
+//  IP Show
+//
+//  Created by Xavier Magallanes on 29/1/24.
+//
+
+import SwiftUI
+
+@main
+struct IP_ShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
